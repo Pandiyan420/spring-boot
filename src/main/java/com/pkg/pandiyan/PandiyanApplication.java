720804796797913c04a3b9869e82fcef394e6b63
pandiyan420 <arunpandiyan273@gmail.com>
2019-11-15 09:56:40 -0800

    First commit

---
+++ src/main/java/com/pkg/pandiyan/PandiyanApplication.java
@@ -0,0 +1,13 @@
+package com.pkg.pandiyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandiyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandiyanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkg/pandiyan/PandiyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkg.pandiyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandiyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
